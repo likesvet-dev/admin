17bc0ff5de3ff29c15a5ed3536b922d0ed943548
Vladimiro Antipin <antipinvladimiro@yandex.ru>
2025-08-12 14:01:46 +0300

    minor ui fix for navbar on small screens

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -37,7 +37,7 @@ export function MainNav({ className, ...props }: React.HTMLAttributes<HTMLElemen
     ];
 
     return (
-        <nav className={cn('flex items-center space-x-4 lg:space-x-6', className)}>
+        <nav className={cn('flex items-center space-x-4 lg:space-x-6 max-[500px]:grid max-[500px]:grid-cols-4 max-[500px]:gap-3 ', className)}>
             {routes.map((route) => (
                 <Link key={route.href} href={route.href} className={cn('text-sm font-medium transition-colors hover:text-primary cursor-pointer', route.active ? 'text-black dark:text-white' : 'text-muted-foreground')}>
                     {route.label}
